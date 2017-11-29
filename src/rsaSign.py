be3238ecea46472370e6d7429d02975a2b83cc10
cnorick <cnathanorick@gmail.com>
2017-11-29 17:27:06 -0500

    Implement certificate creation

--- src/rsaSign.py
+++ src/rsaSign.py
@@ -1,5 +1,6 @@
 from lib.rsa.helpers import Key, modExp 
 from Crypto.Hash import SHA256 as SHA
+import lib.rsa.rsa as rsa
 
 def hash(message, key):
     '''
@@ -54,6 +55,48 @@ def signFromFileToFile(messageFile, keyFile, outFile):
     with open(outFile, 'w') as out:
         out.write(str(sign(message, key)))
 
+def createCert(signer=None):
+    '''
+    Creates a public key, private key, and signature for the thumbprint of the public key, signed by signer.
+    Returns (public key, private key, signature)
+    '''
+    if signer is not None and type(signer) is not Key:
+        raise TypeError('signer must be of type Key or None')
+
+    numBits = 256
+    key = rsa.keygen(numBits)
+    pubKey = Key(key.numBits, key.N, e=key.e)
+    privKey = Key(key.numBits, key.N, d=key.d)
+
+    # If no signer specified, self sign.
+    if signer is None:
+        signer = privKey
+
+    # Check that signer is a private key.
+    if signer.d is None:
+        raise ValueError('signer must be a private key')
+
+    # Sign the thumbprint of the public key file.
+    thumbprint = SHA.new(bytes(str(pubKey), 'utf-8')).digest()
+    sig = sign(thumbprint, privKey)
+
+    return (pubKey, privKey, sig)
+
+
+def createCertToFile(pubKeyFileName, privKeyFileName, signer=None):
+    '''
+    Creates a certificate and saves it to keyFileName. Also creates a thumbprint of the cert, signed by privatekey signer.
+    '''
+    (pubKey, privKey, sig) = createCert(signer)
+    sigFileName = pubKeyFileName + '-casig'
+
+    with open(pubKeyFileName, 'w') as pubKeyFile:
+        pubKeyFile.write(str(pubKey))
+    with open(privKeyFileName, 'w') as privKeyFile:
+        privKeyFile.write(str(privKey))
+    with open(sigFileName, 'w') as sigFile:
+        sigFile.write(str(sig))
+
 def test():
     privKey = Key(12, 3233, d=413)
     pubKey = Key(12, 3233, e=17)
@@ -62,4 +105,5 @@ def test():
     v = validate(s, m, pubKey)
     assert v == True
 
-signFromFileToFile('../test/message','../test/privkey', '../test/out')
+# signFromFileToFile('../test/message','../test/privkey', '../test/out')
+createCertToFile('../test/testcertpub', '../test/testcertpriv')
